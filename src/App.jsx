fbe8e314763d96c64fa5d088e154f6717099d2c9
Jesus M Delgado D <m6nuel@gmail.com>
2023-09-29 08:01:44 -0300

    first commit app ecomerce

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { products } from './mocks/products.json'
+
+function App() {
+  console.log(products)
+
+  return (
+    <>
+      <h1>Ecomerce</h1>
+
+    </>
+  )
+}
+
+export default App
